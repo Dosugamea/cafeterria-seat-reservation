12f5d368b8f1e525935235bfb563b2be4e40a9b7
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2019-06-24 13:36:17 +0900

    Create layout (error/help/term/privacy)

--- Web/routes/page.js
+++ Web/routes/page.js
@@ -1,24 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// トップページ
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 router.get('/index.html', function(req, res, next) {
-  res.render('index');
+  res.redirect('/');
 });
 
+// アナウンス
+router.get('/announcements', function(req, res, next) {
+  res.render('announcements');
+});
+
+// ユーザー
 router.get('/user', function(req, res, next) {
   res.render('mypage');
 });
 
+// 連絡先
 router.get('/contact', function(req, res, next) {
   res.render('contact');
 });
 
+// ヘルプ
 router.get('/help', function(req, res, next) {
   res.render('help');
+});
+
+// 利用規約
+router.get('/term_of_service', function(req, res, next) {
+  res.render('term_of_service');
+});
+
+// プライバシーポリシー
+router.get('/privacy_policy', function(req, res, next) {
+  res.render('privacy_policy');
 });
 
 module.exports = router;
