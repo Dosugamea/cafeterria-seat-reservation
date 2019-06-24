0e89903001318c7f150f231682e91f061fca0407
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2019-06-24 16:04:57 +0900

    Create layout(reserve/reserve_code)

--- Web/routes/page.js
+++ Web/routes/page.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+ ログイン不要ページ
+*/
+
 // トップページ
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -8,35 +12,54 @@ router.get('/', function(req, res, next) {
 router.get('/index.html', function(req, res, next) {
   res.redirect('/');
 });
-
 // アナウンス
 router.get('/announcements', function(req, res, next) {
   res.render('announcements');
 });
-
-// ユーザー
-router.get('/user', function(req, res, next) {
-  res.render('mypage');
-});
-
 // 連絡先
 router.get('/contact', function(req, res, next) {
   res.render('contact');
 });
-
 // ヘルプ
 router.get('/help', function(req, res, next) {
   res.render('help');
 });
-
 // 利用規約
 router.get('/term_of_service', function(req, res, next) {
   res.render('term_of_service');
 });
-
 // プライバシーポリシー
 router.get('/privacy_policy', function(req, res, next) {
   res.render('privacy_policy');
+});
+
+/*
+ ログイン必須ページ
+*/
+
+// ユーザーページ
+router.get('/user', function(req, res, next) {
+  res.render('user');
+});
+// アカウント設定編集
+router.get('/user/edit', function(req, res, next) {
+  res.render('user_edit');
+});
+// ログアウト
+router.get('/user/sign_out', function(req, res, next) {
+  res.render('index');
+});
+// 予約
+router.get('/user/reserve', function(req, res, next) {
+  res.render('reserve');
+});
+// 予約コード表示
+router.get('/user/reserve/code', function(req, res, next) {
+  res.render('reserve_code');
+});
+// 予約状態
+router.get('/user/reserve/status', function(req, res, next) {
+  res.render('reserve_status');
 });
 
 module.exports = router;
