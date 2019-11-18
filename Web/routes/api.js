9bc2be46241bc387cb5451144dc876090c59caa1
Dosugamea@SourceTree <17107514+Dosugamea@users.noreply.github.com>
2019-11-18 18:37:41 +0900

    Implementing WebAPI 予約APIの再検討をした

--- Web/routes/api.js
+++ Web/routes/api.js
@@ -1,28 +1,83 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-var mysql = require('mysql2');
-var connection = mysql.createConnection({
+let express = require('express');
+let router = express.Router();
+let passport = require('passport');
+let mysql = require('mysql2');
+let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'lakito'
 });
 
+/*
+1 使用時間を決めて予約をとる(直前でも可)(席取りはさせない)
+2 行く
+3 かざす その時空いてる席、もしくは終了が一番近い席に案内される
+4 一応基準としてタイマーが動くが先に食べ終わったら離れてQRコードを再度かざすことで
+　　空き時間はなくせる(そのあとの予約では時間を短くしてもらう)
+5 みんなハッピー!!
+
+次に席が空く予定時刻を予約画面に表示するとわかりやすい！
+対応席数を利用時間毎に指定できると良い?
+
+*/
+
+
 // 予約の受付
 router.post('/user/reserve', function(req,res){
-	res.redirect('/user');
+	let userId = req.body.userId;
+	let requestHour = req.body.requestHour;
 });
 
 // 予約のQR読み込み待ち(一定秒数毎に1回たたく)
 router.post('/user/wait_qr', function(req,res){
-	
+	let userId = req.body.userId;
+	let reserveId = req.body.reserveId;
+	connection.query("SELECT 'T' FROM reserves WHERE userId=? AND reserveId=?",[userId,reserveId], function(err,data){
+		if (err){
+			res.json({
+				status:"ng",
+				reason:"database error"
+			});
+		}
+		if (data.length > 0){
+			switch(data[0].status){
+				case 0:
+					res.json({
+						status:"ng",
+						code: "0",
+						reason:"not yet loaded"
+					});
+					break;
+				case 1:
+					res.json({
+						status:"ok",
+						code: "1",
+						reason:"start using seat ok"
+					});
+					break;
+				case 2:
+					res.json({
+						status:"ok",
+						code: "2",
+						reason:"end using seat ok"
+					});
+					break;
+				default:
+					res.json({
+						status:"ng",
+						code: "9",
+						reason:"unknown response"
+					});
+			}
+		}
+	});
 });
 
 // 予約の検証(QRコード読み取り端末から受付)
 router.post('/admin/verify_reserve', function(req,res){
-	var userId = req.body.userId;
-	var reserveId = req.body.reserveId;
+	let userId = req.body.userId;
+	let reserveId = req.body.reserveId;
 	console.log(userId);
 	console.log(reserveId);
 	// 予約データ取得
@@ -30,19 +85,19 @@ router.post('/admin/verify_reserve', function(req,res){
 		// 取得できたら
 		if (data.length > 0){
 			//今の時間
-			var nowTime = new Date();
-			var fTime = new Date(data[0].fTime);
-			var diffTime = Math.abs(nowTime-fTime);
-			var diffMins = Math.round(((diffTime % 86400000) % 3600000) / 60000);
+			let nowTime = new Date();
+			let fTime = new Date(data[0].fTime);
+			let diffTime = Math.abs(nowTime-fTime);
+			let diffMins = Math.round(((diffTime % 86400000) % 3600000) / 60000);
 			console.log(diffMins);
-			var status_value = "";
-			var reason_value = "";
+			let status_value = "";
+			let reason_value = "";
 			//時間を過ぎている
-			if (diffMins < -3){
+			if (diffMins < -60){
 				status_value = "ng";
 				reason_value = "the reserve is time out.";
 			//まだ時間じゃない
-			}else if (diffMins > 5){
+			}else if (diffMins > 3){
 				status_value = "ng";
 				reason_value = "the reserve is not in time.";
 			// 状態問題なし
@@ -59,12 +114,10 @@ router.post('/admin/verify_reserve', function(req,res){
 				status_value = "ng";
 				reason_value = "the reserve is disabled by admin";
 			}
-			//DBを更新する
-			connection.query("UPDATE `reserves` SET `qr_response`=? WHERE `reserveId`=?;",[reason,reserveId], function(err, data) {
-				res.json({
-						status:　status_value,
-						reason: reason_value
-				});
+			//返事を返す
+			res.json({
+				status:　status_value,
+				reason: reason_value
 			});
 		// 取得できなければ
 		}else{
